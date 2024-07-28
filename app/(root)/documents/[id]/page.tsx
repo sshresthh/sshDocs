3657aec5518be473bf373757b15353668a9e93dc
ss.stha00 <ss.stha00@gmail.com>
2024-07-28 13:16:26 +0000

    (updated) logo and the page

--- app/(root)/documents/[id]/page.tsx
+++ app/(root)/documents/[id]/page.tsx
@@ -1,9 +1,15 @@
+import Header from "@/components/Header";
 import { Editor } from "@/components/editor/Editor";
 import React from "react";
 
 const Document = () => {
   return (
     <div>
+      <Header>
+        <div className="flex ">
+
+        </div>
+      </Header>
       <Editor />
     </div>
   );
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,29 +1,29 @@
-import Image from 'next/image'
-import Link from 'next/link'
-import React from 'react'
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
 
-const Header = ({children} : HeaderProps) => {
+const Header = ({ children }: HeaderProps) => {
   return (
     <div className="header">
-        <Link href="/" className="md:flex-1">
-        <Image 
-            src="/assets/icons/logo.svg"
-            width={120}
-            height={32}
-            alt="logo"
-            className="hidden md:block"
+      <Link href="/" className="md:flex-1">
+        <Image
+          src="/assets/icons/logo.svg"
+          width={184}
+          height={32}
+          alt="logo"
+          className="hidden md:block"
         />
-        <Image 
-            src="/assets/icons/logo-icon.svg"
-            width={32}
-            height={32}
-            alt="logo"
-            className="mr-2 md:hidden"
+        <Image
+          src="/assets/icons/logo-icon.svg"
+          width={110}
+          height={40}
+          alt="logo"
+          className="mr-2 md:hidden"
         />
-        </Link>
-        {children}
+      </Link>
+      {children}
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
