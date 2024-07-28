7d92d0a83c63b31ec5891e4c0cd25af1d4bc6fbe
Shyamsundar Shrestha <ss.stha00@gmail.com>
2024-07-29 03:43:55 +0930

    (Updated) page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,8 @@ export default function Home() {
   return (
     <main className="">
       <div>
-        <h1>Hello World</h1>
-        <Button className="bg-red-500 hover:bg-blue-500">Button</Button>
+        <h1>sshDocs</h1>
+        <Button asChild Link="/documents/1" className="bg-red-500 hover:bg-blue-500">Click Me</Button>
       </div>
     </main>
   );
