5664fd90de48da60883bf839e4b43ff2dee6d8d7
ss.stha00 <ss.stha00@gmail.com>
2024-07-28 13:50:49 +0000

    (update) working till here

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,31 +1,45 @@
-import "@/app/globals.css"
-import { Inter as FontSans } from "next/font/google"
+import "@/app/globals.css";
+import { Inter as FontSans } from "next/font/google";
 
-import { cn } from "@/lib/utils"
-import { Metadata } from "next"
+import { cn } from "@/lib/utils";
+import { Metadata } from "next";
+
+import {
+  ClerkProvider,
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from "@clerk/nextjs";
 
 const fontSans = FontSans({
   subsets: ["latin"],
   variable: "--font-sans",
-})
+});
 
 export const metadata: Metadata = {
   title: "sshDocs",
-  description: "google docs clone"
-}
+  description: "google docs clone",
+};
 
-export default function RootLayout({ children }: {children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
-    <html lang="en" suppressHydrationWarning>
-      <head />
-      <body
-        className={cn(
-          "min-h-screen bg-background font-sans antialiased",
-          fontSans.variable
-        )}
-      >
-        {children}
-      </body>
-    </html>
-  )
+    <ClerkProvider>
+      <html lang="en" suppressHydrationWarning>
+        <head />
+        <body
+          className={cn(
+            "min-h-screen bg-background font-sans antialiased",
+            fontSans.variable
+          )}
+        >
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
+  );
 }
