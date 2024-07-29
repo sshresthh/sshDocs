abbc13a91dcf13846fcf8f7921b6ab6f913767d2
Shyamsundar Shrestha <ss.stha00@gmail.com>
2024-07-29 13:15:43 +0930

    (idk)

--- app/(root)/documents/[id]/page.tsx
+++ app/(root)/documents/[id]/page.tsx
@@ -1,13 +1,12 @@
 import Header from "@/components/Header";
 import { Editor } from "@/components/editor/Editor";
-import React from "react";
 
 const Document = () => {
   return (
     <div>
       <Header>
         <div className="flex w-fit items-center justiry-center gap-2">
-            <p className="document-title">Document Title</p>
+          <p className="document-title">Document Title</p>
         </div>
       </Header>
       <Editor />
--- app/layout.tsx
+++ app/layout.tsx
@@ -4,14 +4,6 @@ import { Inter as FontSans } from "next/font/google";
 import { cn } from "@/lib/utils";
 import { Metadata } from "next";
 
-import {
-  ClerkProvider,
-  SignInButton,
-  SignedIn,
-  SignedOut,
-  UserButton,
-} from "@clerk/nextjs";
-
 const fontSans = FontSans({
   subsets: ["latin"],
   variable: "--font-sans",
@@ -28,18 +20,16 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <ClerkProvider>
-      <html lang="en" suppressHydrationWarning>
-        <head />
-        <body
-          className={cn(
-            "min-h-screen bg-background font-sans antialiased",
-            fontSans.variable
-          )}
-        >
-          {children}
-        </body>
-      </html>
-    </ClerkProvider>
+    <html lang="en" suppressHydrationWarning>
+      <head />
+      <body
+        className={cn(
+          "min-h-screen bg-background font-sans antialiased",
+          fontSans.variable
+        )}
+      >
+        {children}
+      </body>
+    </html>
   );
 }
