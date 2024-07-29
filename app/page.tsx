cbf82ae408dd283b959efdb7d8055d07bbcb5c15
Shyamsundar Shrestha <ss.stha00@gmail.com>
2024-07-30 00:24:05 +0930

    (fixed) the layout

--- app/page.tsx
+++ app/page.tsx
@@ -4,10 +4,9 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main className="">
-      <div>
-        <h1>sshDocs</h1>
-        <Button asChild>
-          <Link href="/documents/nayan">Click Me</Link>
+      <div className="mt-48 text-center justify-center items-center">
+        <Button asChild className="bg-cyan-400">
+          <Link href="/documents/nayan" className="text-blue-950">Click Me</Link>
         </Button>
       </div>
     </main>
